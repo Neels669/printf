3094b813e8723a403ca3dc09915b62f3fd7fefde
Neelam <neelam_669@yahoo.com>
2023-06-27 06:15:26 -0700

    Amend

--- main.h
+++ main.h
@@ -3,14 +3,28 @@
 
 #include <stdio.h>
 #include <limits.h>
-
+#include <stdlib.h>
 #include <stdarg.h>
 #include <stdint.h>
+#include <unistd.>
 
 int _printf(const char *format, ...);
-int _putchar(char c);
-int _char(va_list args);
-int _string(va_list args);
-int _percent(va_list);
+int (*specifiers(const char *format))(va_list);
+int print_char(va_list);
+int print_str(va_list);
+int print_cent(va_list);
+int print_integer(va_list);
+int print_decimal(va_list);
+
+/**
+ * struct fun - specifiers to pointer
+ * @t: char to comare it
+ * @f: function to print
+ */
+typedef struct fun
+{
+	char *t;
+	int (*f)(va_list);
+} fun_t;
 
 #endif
